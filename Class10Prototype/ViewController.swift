6b0db81165d644e5aef64bb2dab0d069625a01c6
Sheng-Han Chen <richard@richards-macbook-pro.local>
2019-03-05 19:32:17 +0800

    Initial Commit

---
+++ Class10Prototype/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Class10Prototype
+//
+//  Created by Richard Chen on 2019/3/5.
+//  Copyright © 2019 Richard Chen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
